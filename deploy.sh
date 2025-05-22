d30e24cb0957fe6fdfa965290f6cfe356b18e426
xiayinpeng <xiayinpeng@maetimes.com>
2025-05-22 14:06:04 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,15 @@
 echo "🔁 拉取最新代码..."
 git pull origin master
 
+echo "🧹 清理旧的缓存..."
+rm -rf .output/cache/*
 
 echo "🏗️ 重新构建项目..."
 npm run build
+
+echo "📂 设置缓存目录权限..."
+mkdir -p .output/cache
+chmod -R 755 .output/cache
 
 echo "🚀 重启 PM2 服务..."
 pm2 restart nuxt3_demo
